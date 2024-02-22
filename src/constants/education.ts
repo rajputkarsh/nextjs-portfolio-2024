baccbd73edf37360b6cc02a7b05acd27deea3eb2
utkarsh <r.utkarsh.0010@gmail.com>
2024-02-22 23:57:41 +0530

    update education timeline content

--- src/constants/education.ts
+++ src/constants/education.ts
@@ -1,5 +1,9 @@
 import BabyFace from "@/assets//icons/baby-face.svg";
 import School from "@/assets//icons/school.svg";
+import College from "@/assets//icons/college.svg";
+import GraduationHat from "@/assets//icons/graduation-hat.svg";
+import Graduation from "@/assets//icons/graduation.svg";
+import PartyPopper from "@/assets//icons/party-popper.svg";
 
 const EDUCATION_TIMELINE = [
   {
@@ -13,7 +17,7 @@ const EDUCATION_TIMELINE = [
   {
     index: 2,
     event: "Became Matriculate",
-    icon: "teenyicons:school-outline",
+    icon: School.src,
     date: "2015",
     content:
       "Completed my matriculation examination in 2015 with a score of 9.6 CGPA",
@@ -22,7 +26,7 @@ const EDUCATION_TIMELINE = [
   {
     index: 3,
     event: "Became a High School Graduate",
-    icon: "bxs:school",
+    icon: College.src,
     date: "2017",
     content:
       "Completed my AISSCE (12th Standard) examination in 2015 with a score of 83.4%",
@@ -31,7 +35,7 @@ const EDUCATION_TIMELINE = [
   {
     index: 4,
     event: "Became an Engineer",
-    icon: "material-symbols:school-outline-rounded",
+    icon: GraduationHat.src,
     date: "2021",
     content:
       "Completed my Bachelors in Technology (Information Technology) in 2021 with an accumulative score of 8.48 CGPA",
@@ -40,7 +44,7 @@ const EDUCATION_TIMELINE = [
   {
     index: 5,
     event: "Started specializing in Data Structures and Algorithms",
-    icon: "fluent-mdl2:learning-tools",
+    icon: Graduation.src,
     date: "2022",
     content: `Started my journey in Competitive Coding and DSA via Scaler Academy's Problem Solving & System Design course. Joined the September 2022 intake batch.`,
     contentIcon: SCALER_LOGO,
@@ -48,7 +52,7 @@ const EDUCATION_TIMELINE = [
   {
     index: 6,
     event: "PRESENT",
-    icon: "bx:party",
+    icon: PartyPopper.src,
     date: new Date().getFullYear().toString(),
     content: "",
     contentIcon: "",
