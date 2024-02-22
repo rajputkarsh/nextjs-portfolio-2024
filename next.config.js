dec0bc1297b1dd4505ce9604070e4f12c459a5d3
utkarsh <r.utkarsh.0010@gmail.com>
2024-02-23 00:17:04 +0530

    add education timeline

--- next.config.js
+++ next.config.js
@@ -11,20 +11,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-        // for webpack 5 use
-        // { and: [/\.(js|ts)x?$/] }
-      },
-
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
--- src/containers/education/EducationTimeline.tsx
+++ src/containers/education/EducationTimeline.tsx
@@ -1,5 +1,13 @@
+import Timeline from "@/components/timeline";
+import config from "@/constants/config";
+import EDUCATION_TIMELINE from "@/constants/education";
+
 function EducationTimeline() {
-  return <div>EducationTimeline</div>;
+  return (
+    <div className="w-full">
+      <Timeline events={EDUCATION_TIMELINE} />
+    </div>
+  );
 }
 
 export default EducationTimeline;
