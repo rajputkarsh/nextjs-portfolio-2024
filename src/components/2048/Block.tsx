d565c852b071e7fbadda1deb812abe350bc13ad3
utkarsh <r.utkarsh.0010@gmail.com>
2024-02-25 03:42:06 +0530

    add animations to 2048 game block

--- src/components/2048/Block.tsx
+++ src/components/2048/Block.tsx
@@ -10,7 +10,7 @@ function Block({ num }: BlockProps) {
     <div
       className={`bg-${getColor(
         num
-      )} text-${getTextColor()} h-20 w-20 m-1 flex justify-center items-center text-5xl font-bold`}
+      )} text-${getTextColor()} h-20 w-20 m-1 flex justify-center items-center text-5xl font-bold transition transform duration-300`}
     >
       {num !== 0 ? num : ""}
     </div>
