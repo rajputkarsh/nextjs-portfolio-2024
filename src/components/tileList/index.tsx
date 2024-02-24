5821f15a1101ff6232551cc37c2a89cd12ef1e11
utkarsh <r.utkarsh.0010@gmail.com>
2024-02-25 02:23:06 +0530

    create common tilelist component

---
+++ src/components/tileList/index.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { Game } from "@/interfaces/game";
+import { Project } from "@/interfaces/project";
+import Link from "next/link";
+
+interface TileListProps {
+  list: Array<Game | Project>;
+}
+
+const isGame = (data: Game | Project): data is Game => "url" in data;
+const isProject = (data: Game | Project): data is Project => "demoUrl" in data;
+
+function TileList<T>({ list }: TileListProps) {
+  return (
+    <>
+      {list.map((item) => {
+        <Link
+          key={item.title}
+          className="bg-slate-200 p-4 border-2 border-slate-500 rounded-2xl w-full md:w-3/12"
+          href={isGame(item) ? item?.url : item?.demoUrl || item?.sourceUrl}
+          target="_blank"
+        >
+          <h4 className="text-theme-color font-semibold text:md md:text-xl mb-4">
+            {item.title}
+          </h4>
+          <Image
+            className="mb-4 w-full max-h-40 object-contain m-auto"
+            src={item.image}
+            alt=""
+            width={400}
+            height={400}
+          />
+          <p className="">{item.body}</p>
+        </Link>;
+      })}
+    </>
+  );
+}
+
+export default TileList;
--- src/containers/game/GameList.tsx
+++ src/containers/game/GameList.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { fetchGameList } from "@/actions/game";
 import config from "@/constants/config";
 import { Game } from "@/interfaces/game";
 import loader from "@/assets/loader.svg";
+import TileList from "@/components/tileList";
 
 function GameList() {
   const [gameList, setGameList] = useState<Array<Game>>();
@@ -25,26 +25,7 @@ function GameList() {
 
       {gameList ? (
         <div className="w-full flex flex-row flex-wrap justify-center gap-4 px-8 md:px-16">
-          {gameList.map((game) => (
-            <Link
-              key={game.title}
-              className="bg-slate-200 p-4 border-2 border-slate-500 rounded-2xl w-full md:w-3/12"
-              href={game?.url}
-              target="_blank"
-            >
-              <h4 className="text-theme-color font-semibold text:md md:text-xl mb-4">
-                {game.title}
-              </h4>
-              <Image
-                className="mb-4 w-full max-h-40 object-contain m-auto"
-                src={game.image}
-                alt=""
-                width={400}
-                height={400}
-              />
-              <p className="">{game.body}</p>
-            </Link>
-          ))}
+          <TileList<Game> list={gameList} />
         </div>
       ) : (
         <Image
