d245dba783ed132f0807ea4b707686c7d600ec3f
utkarsh <r.utkarsh.0010@gmail.com>
2024-02-27 05:43:23 +0530

    add hotjar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,11 +4,23 @@ import Script from "next/script";
 function Header() {
   const nonce = headers().get("x-nonce") || undefined;
   return (
-    <Script
-      src="https://www.googletagmanager.com/gtag/js"
-      strategy="afterInteractive"
-      nonce={nonce}
-    />
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js"
+        strategy="afterInteractive"
+        nonce={nonce}
+      />
+      <Script id="hotjar-analytics">
+        {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3882494,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
+    </>
   );
 }
 
