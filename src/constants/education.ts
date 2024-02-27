94a1e2ffe3aea3cddb777bd79c0ea199b0cf21bb
Utkarsh <r.utkarsh.0010@gmail.com>
2024-02-27 14:16:27 +0530

    Update education.ts

--- src/constants/education.ts
+++ src/constants/education.ts
@@ -22,7 +22,7 @@ const EDUCATION_TIMELINE = [
     index: 2,
     event: "Became Matriculate",
     icon: School.src,
-    date: "2015",
+    date: "2013",
     content:
       "Completed my matriculation examination in 2015 with a score of 9.6 CGPA",
     contentIcon: BcmLogo.src,
@@ -31,7 +31,7 @@ const EDUCATION_TIMELINE = [
     index: 3,
     event: "Became a High School Graduate",
     icon: College.src,
-    date: "2017",
+    date: "2015",
     content:
       "Completed my AISSCE (12th Standard) examination in 2015 with a score of 83.4%",
     contentIcon: BcmLogo.src,
@@ -40,7 +40,7 @@ const EDUCATION_TIMELINE = [
     index: 4,
     event: "Became an Engineer",
     icon: GraduationHat.src,
-    date: "2021",
+    date: "2019",
     content:
       "Completed my Bachelors in Technology (Information Technology) in 2021 with an accumulative score of 8.48 CGPA",
     contentIcon: DavietLogo.src,
