34cc70e8cbf9deb4a208b51db8d62d91fc90a1bc
Utkarsh <r.utkarsh.0010@gmail.com>
2024-03-10 23:10:57 +0530

    Update HomeIntroduction.tsx

--- src/containers/home/HomeIntroduction.tsx
+++ src/containers/home/HomeIntroduction.tsx
@@ -8,7 +8,7 @@ function HomeIntroduction() {
   return (
     <div className="flex flex-col lg:flex-row px-4 sm:px-8 md:px-12">
       <div className="h-1/2 lg:h-full lg:w-1/2 relative text-center flex flex-row justify-center">
-        <Image src={CodingBoy.src} alt="" width={700} height={700} />
+        <Image src={CodingBoy.src} alt="" width={700} height={700} loading="eager"/>
       </div>
       <div className="mt-12 md:mt-0 lg:w-1/2 flex flex-col gap-8 items-center justify-center">
         <h1 className="text-3xl md:text-4xl font-bold flex flex-row gap-2">
