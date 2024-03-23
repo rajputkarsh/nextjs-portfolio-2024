ca5355ac8c0c9e6955ad5f9671f6a05ca9c68413
utkarsh <r.utkarsh.0010@gmail.com>
2024-03-23 17:06:13 +0530

    fix dark theme issue on refresh

--- src/hooks/useCurrentTheme.tsx
+++ src/hooks/useCurrentTheme.tsx
@@ -3,6 +3,9 @@ import { useTheme } from "next-themes";
 const useCurrentTheme = () => {
   let { theme, systemTheme, setTheme } = useTheme();
   if (theme === "system") theme = systemTheme;
+  if (!theme) {
+    theme = localStorage.getItem('theme') || 'light';
+  }
 
   return { theme: theme as string, setTheme };
 };
