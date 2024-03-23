ecf2a24c4cc95ae9b0233b3d08242e3d130451f4
utkarsh <r.utkarsh.0010@gmail.com>
2024-03-23 15:27:09 +0530

    WIP: theme

--- src/components/themeButton/index.tsx
+++ src/components/themeButton/index.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+"use client";
+
+import useCurrentTheme from '@/hooks/useCurrentTheme'
+import React, { useEffect } from 'react'
 
 function ThemeButton() {
+  const { theme, setTheme } = useCurrentTheme();
+
+  useEffect(() => {
+    setTheme('dark');
+  }, [])
+
   return (
     <button className='fixed z-50 bottom-8 right-8'>Change Theme</button>
   )
