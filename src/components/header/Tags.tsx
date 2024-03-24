ee26aea16de1ffa0a67126e44eabf8addcc88545
utkarsh <r.utkarsh.0010@gmail.com>
2024-03-24 16:03:53 +0530

    update theme color

--- src/components/header/Tags.tsx
+++ src/components/header/Tags.tsx
@@ -2,10 +2,10 @@ function Tags() {
   return (
     <>
       <link rel="manifest" href="manifest.json" />
-      <meta name="theme-color" content="#5f5eaa" />
-      <meta name="msapplication-TileColor" content="#5f5eaa" />
-      <meta name="msapplication-navbutton-color" content="#5f5eaa" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="#5f5eaa" />
+      <meta name="theme-color" content="#e3405f" />
+      <meta name="msapplication-TileColor" content="#e3405f" />
+      <meta name="msapplication-navbutton-color" content="#e3405f" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="#e3405f" />
     </>
   );
 }
