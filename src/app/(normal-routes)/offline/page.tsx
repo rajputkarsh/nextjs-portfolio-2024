110c7451e5bfec3a85f5616f07baae1813196eaf
utkarsh <r.utkarsh.0010@gmail.com>
2024-03-24 16:39:34 +0530

    update offline page

--- src/app/(normal-routes)/offline/page.tsx
+++ src/app/(normal-routes)/offline/page.tsx
@@ -5,18 +5,18 @@ import Typewriter from "typewriter-effect";
 function Offline() {
   return (
     <div className="w-screen h-[91vh] md:h-[90.5vh] flex flex-col gap-8 justify-center items-center">
-      <h3 className="text-4xl font-semibold underline-animation-black">
-        Oops! It seems that you're offline.
+      <h3 className="text-3xl md:text-4xl px-2 font-semibold underline-animation-black flex flex-row justify-center">
+        Oops! It seems you're offline
       </h3>
-      <h3 className="text-3xl font-semibold flex flex-row gap-2">
+      <h3 className="w-full px-4 text-2xl md:text-3xl font-semibold flex flex-row gap-2 justify-center">
         Retrying
-          <Typewriter
-            options={{
-              strings: "...",
-              autoStart: true,
-              loop: true,
-            }}
-          />
+        <Typewriter
+          options={{
+            strings: "...",
+            autoStart: true,
+            loop: true,
+          }}
+        />
       </h3>
     </div>
   );
