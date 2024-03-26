e3fb96882f553c9af46e042dbfee157b2a641214
utkarsh <r.utkarsh.0010@gmail.com>
2024-03-27 04:18:32 +0530

    update code

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,63 +1,65 @@
-
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("../firebase-messaging-sw.js")
     .then(function (registration) {
+      registration.addEventListener("installed", (event) => {
+        // if (event.isUpdate) {
+        console.log(`event -- `, event);
+        console.log(`self -- `, self);
+        console.log(`self.clients -- `, self.clients);
+        console.log(`self.windowClientId -- `, self.windowClientId);
+        self.clients
+          .get(self.windowClientId)
+          .postMessage({ type: "installed" });
+        // }
+      });
+
+      registration.addEventListener("activated", (event) => {
+        event.waitUntil(
+          self.clients.matchAll().then((clients) => {
+            clients.forEach((client) => {
+              client.postMessage(
+                `Log from service worker: ${JSON.stringify(event)}`
+              );
+            });
+          })
+        );
+      });
+
+      registration.addEventListener("push", (event) => {
+        try {
+          console.log(`event.data == `, event.data);
+          const data = JSON.parse(event.data);
+          console.log(`data == `, data);
+
+          const title = data?.title || "Utkarsh";
+
+          const options = {
+            body: data.body,
+            icon: data.icon,
+            data: {
+              url: data.url,
+            },
+          };
+          event.waitUntil(self.registration.showNotification(title, options));
+        } catch (error) {
+          console.log('Error in SW "PUSH" - ', error);
+        }
+      });
+
+      registration.addEventListener("notificationclick", (event) => {
+        try {
+          const notificationData = event.notification.data;
+
+          if (notificationData.url) {
+            clients.openWindow(notificationData.url);
+          }
+
+          event.notification.close();
+        } catch (error) {
+          console.log('Error in SW "PUSH" - ', error);
+        }
+      });
     })
     .catch(function (err) {});
 }
-
-self.addEventListener("installed", (event) => {
-  // if (event.isUpdate) {
-    console.log(`event -- `, event);
-    console.log(`self -- `, self);
-    console.log(`self.clients -- `, self.clients);
-    console.log(`self.windowClientId -- `, self.windowClientId);
-    self.clients.get(self.windowClientId).postMessage({ type: "installed" });
-  // }
-});
-
-self.addEventListener("activated", (event) => {
-  event.waitUntil(
-    self.clients.matchAll().then((clients) => {
-      clients.forEach((client) => {
-        client.postMessage(`Log from service worker: ${JSON.stringify(event)}`);
-      });
-    })
-  );
-});
-
-self.addEventListener("push", (event) => {
-  try {
-    console.log(`event.data == `, event.data);
-    const data = JSON.parse(event.data);
-    console.log(`data == `, data);
-
-    const title = data?.title || "Utkarsh";
-
-    const options = {
-      body: data.body,
-      icon: data.icon,
-      data: {
-        url: data.url,
-      },
-    };
-    event.waitUntil(self.registration.showNotification(title, options));
-  } catch (error) {
-    console.log('Error in SW "PUSH" - ', error);
-  }
-});
-
-self.addEventListener("notificationclick", (event) => {
-  try {
-    const notificationData = event.notification.data;
-
-    if (notificationData.url) {
-      clients.openWindow(notificationData.url);
-    }
-
-    event.notification.close();
-  } catch (error) {
-    console.log('Error in SW "PUSH" - ', error);
-  }
-});
