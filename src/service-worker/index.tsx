7876ca056195e74ec54acc8cf95ab9869db542ab
utkarsh <r.utkarsh.0010@gmail.com>
2024-03-28 21:45:28 +0530

    update service worker installation logic

--- src/service-worker/index.tsx
+++ src/service-worker/index.tsx
@@ -1,29 +1,16 @@
 import registerFirebaseServiceWorker from "./firebase";
 import registerWorkboxServiceWorker from "./workbox";
 
-async function waitForServiceWorkerReady() {
-  if ("serviceWorker" in navigator) {
-    const registrations = await navigator.serviceWorker.getRegistrations();
-    const promises = registrations.map(
-      (registration) => registration.waiting || registration.active
-    );
-    await Promise.all(promises);
-  }
-}
-
 export default async function registerServiceWorkers() {
-  console.log(`waiting for service workers`);
-  waitForServiceWorkerReady()
-    .then(() => {
-      console.log(`1`);
-      registerFirebaseServiceWorker().then(() => {
-        console.log(`2`);
-        registerWorkboxServiceWorker()?.then(() => {
-          console.log(`3`);
-        });
-      });
-    })
-    .catch((error) => {
-      console.error("Error while waiting for service workers:", error);
-    });
+  try {
+    console.log(`1`);
+    setTimeout(async () => {
+      await registerFirebaseServiceWorker();
+    }, 30 * 1000)
+    console.log(`2`);
+    await registerWorkboxServiceWorker();
+    console.log(`3`);
+  } catch(error) {
+    console.log(`error while adding service worker - `, error);
+  }
 }
