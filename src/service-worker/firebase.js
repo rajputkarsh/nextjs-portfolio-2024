50ed6dff3d43ccce52764fa5ffa21c1f511976e4
utkarsh <r.utkarsh.0010@gmail.com>
2024-03-28 22:55:12 +0530

    update service worker installation logic

--- src/service-worker/firebase.js
+++ src/service-worker/firebase.js
@@ -4,18 +4,18 @@ export default function registerFirebaseServiceWorker() {
   return new Promise((resolve) => {
     if ("serviceWorker" in navigator) {
       Notification.requestPermission().then((permission) => {
-        console.log(`registering service worker`);
-        navigator.serviceWorker
-          .register("../firebase-messaging-sw.js")
-          .then(function (registration) {
-            console.log(`fb sw registered`);
-            navigator.serviceWorker.ready.then((serviceWorker) => {
-              console.log(`fb sw ready`);
-              const firebase = new Firebase(false);
-              if (permission === "granted") {
-                console.log(`permission granted`);
+        console.log(`permission - `, permission);
+        if (permission === "granted") {
+          console.log(`registering service worker`);
+          navigator.serviceWorker
+            .register("../firebase-messaging-sw.js")
+            .then(function (registration) {
+              console.log(`fb sw registered - `, registration);
+              navigator.serviceWorker.ready.then((serviceWorker) => {
+                console.log(`fb sw ready - `, serviceWorker);
+                const firebase = new Firebase(false);
                 navigator.serviceWorker.ready.then((ready) => {
-                  console.log(`awaited SW`);
+                  console.log(`awaited SW - `, ready);
                   firebase.getMessagingToken().then((token) => {
                     console.log(`adding FCM token -- `, token);
                     firebase.onMessageCallback((payload) => {
@@ -23,9 +23,9 @@ export default function registerFirebaseServiceWorker() {
                     });
                   });
                 });
-              }
+              });
             });
-          });
+        }
       });
     }
     resolve(true);
