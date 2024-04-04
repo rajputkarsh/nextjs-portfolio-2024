4a6e92a18115337a86b63e06b719bb8409714f9d
utkarsh <r.utkarsh.0010@gmail.com>
2024-04-04 22:05:40 +0530

    update PWAInstallHandler

--- src/components/pwaInstallHandler/index.tsx
+++ src/components/pwaInstallHandler/index.tsx
@@ -1,7 +1,57 @@
-'use client';
+"use client";
 
+import { useEffect, useState } from "react";
+import InstallPWADialog from "../Dialogs/InstallPWA";
+
+let deferredPrompt: any;
+let timer: NodeJS.Timeout;
+const POPUP_TIME = 30;
 function PWAInstallHandler() {
-  return <></>  
+  const [time, setTime] = useState<number>(POPUP_TIME);
+  const [showDialog, setShowDialog] = useState<boolean>(false);
+
+  const handler = (e: Event) => {
+    e.preventDefault();
+    deferredPrompt = e;
+  };
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", handler);
+
+    timer = setInterval(() => {
+      setTime((p) => {
+        if (p > 0) {
+          return p - 1;
+        } else {
+          clearInterval(timer);
+          setShowDialog(true);
+          return 0;
+        }
+      });
+    }, 1000);
+
+        return () => {
+          window.removeEventListener("beforeinstallprompt", handler);
+        };
+  }, []);
+
+  const installPWA = async () => {
+    if(deferredPrompt) {
+      const outcome = await deferredPrompt.prompt();
+    }
+  }
+
+  const closeDialog = () => {
+    setShowDialog(false);
+  }
+
+  if (showDialog && time < 1) {
+    return (
+      <InstallPWADialog installPWA={installPWA} closeDialog={closeDialog} />
+    );
+  }
+
+  return <></>;
 }
 
-export default PWAInstallHandler
+export default PWAInstallHandler;
