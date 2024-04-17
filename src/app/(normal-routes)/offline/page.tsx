fc55a669abef7694dc9e894604fa54a74f42dd18
Utkarsh <r.utkarsh.0010@gmail.com>
2024-04-17 15:06:42 +0530

    fix eslint errors

--- src/app/(normal-routes)/offline/page.tsx
+++ src/app/(normal-routes)/offline/page.tsx
@@ -6,7 +6,7 @@ function Offline() {
   return (
     <div className="w-screen h-[91vh] md:h-[90.5vh] flex flex-col gap-8 justify-center items-center">
       <h3 className="text-3xl md:text-4xl px-2 font-semibold underline-animation-black flex flex-row justify-center">
-        Oops! It seems you're offline
+        Oops! It seems you&apos;re offline
       </h3>
       <h3 className="w-full px-4 text-2xl md:text-3xl font-semibold flex flex-row gap-2 justify-center">
         Retrying
