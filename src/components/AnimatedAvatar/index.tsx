218c86de370d2db71a7f30248e63451418a66fbb
utkarsh <r.utkarsh.0010@gmail.com>
2024-05-18 12:45:42 +0530

    add 3d model

--- src/components/AnimatedAvatar/index.tsx
+++ src/components/AnimatedAvatar/index.tsx
@@ -1,15 +1,157 @@
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import Model from "./Model";
+"use client";
 
-export default function App() {
+import * as THREE from "three";
+import { GLTF, GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { useEffect } from "react";
+
+export default function AnimatedAvatar() {
+  function loadModel() {
+    const loader = new GLTFLoader();
+    loader.load(
+      "utkarsh.glb",
+      (gltf) => {
+        setupScene(gltf);
+        (document.getElementById("avatar-loading") as HTMLElement).style.display = "none";
+      },
+      (xhr) => {
+        const percentCompletion = Math.round((xhr.loaded / xhr.total) * 100);
+        (document.getElementById(
+          "avatar-loading"
+        ) as HTMLElement).innerText = `LOADING... ${percentCompletion}%`;
+      },
+      (error) => {}
+    );
+  }
+
+  function setupScene(gltf: GLTF) {
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true,
+    });
+    renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+    const container = document.getElementById("avatar-container") as HTMLElement;
+    renderer.setSize(container.clientWidth, 600);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    container.appendChild(renderer.domElement);
+
+    // Camera setup
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      container.clientWidth / container.clientHeight
+    );
+    camera.position.set(0.2, 0.5, 1);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.enablePan = false;
+    controls.enableZoom = false;
+    controls.minDistance = 3;
+    controls.minPolarAngle = 1.4;
+    controls.maxPolarAngle = 1.4;
+    controls.target = new THREE.Vector3(0, 0.75, 0);
+    controls.update();
+
+    // Scene setup
+    const scene = new THREE.Scene();
+
+    // Lighting setup
+    scene.add(new THREE.AmbientLight());
+
+    const spotlight = new THREE.SpotLight(0xffffff, 20, 8, 1);
+    spotlight.penumbra = 0.5;
+    spotlight.position.set(0, 4, 2);
+    spotlight.castShadow = true;
+    scene.add(spotlight);
+
+    const keyLight = new THREE.DirectionalLight(0xffffff, 2);
+    keyLight.position.set(1, 1, 2);
+    keyLight.lookAt(new THREE.Vector3());
+    scene.add(keyLight);
+
+    // Load avatar
+    const avatar = gltf.scene;
+    avatar.traverse((child: any) => {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+    scene.add(avatar);
+
+    // Create pedestal
+    const groundGeometry = new THREE.CylinderGeometry(0.6, 0.6, 0.1, 64);
+    const groundMaterial = new THREE.MeshStandardMaterial();
+    const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+    groundMesh.castShadow = false;
+    groundMesh.receiveShadow = true;
+    groundMesh.position.y -= 0.05;
+    scene.add(groundMesh);
+
+    // Load animations
+    const mixer = new THREE.AnimationMixer(avatar);
+    const clips = gltf.animations;
+    const waveClip = THREE.AnimationClip.findByName(clips, "standing happy");
+    const stumbleClip = THREE.AnimationClip.findByName(clips, "stumble");
+    const waveAction = mixer.clipAction(waveClip);
+    const stumbleAction = mixer.clipAction(stumbleClip);
+
+    let isStumbling = false;
+    const raycaster = new THREE.Raycaster();
+    container.addEventListener("mousedown", (ev) => {
+      const coords = {
+        x: (ev.offsetX / container.clientWidth) * 2 - 1,
+        y: -(ev.offsetY / container.clientHeight) * 2 + 1,
+      } as THREE.Vector2;
+
+      raycaster.setFromCamera(coords, camera);
+      const intersections = raycaster.intersectObject(avatar);
+
+      if (intersections.length > 0) {
+        if (isStumbling) return;
+
+        isStumbling = true;
+        stumbleAction.reset();
+        stumbleAction.play();
+        waveAction.crossFadeTo(stumbleAction, 0.3, false);
+
+        setTimeout(() => {
+          waveAction.reset();
+          waveAction.play();
+          stumbleAction.crossFadeTo(waveAction, 1, false);
+          setTimeout(() => (isStumbling = false), 1000);
+        }, 4000);
+      }
+    });
+
+    window.addEventListener("resize", () => {
+      camera.aspect = container.clientWidth / container.clientHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(container.clientWidth, container.clientHeight);
+    });
+
+    const clock = new THREE.Clock();
+    function animate() {
+      requestAnimationFrame(animate);
+      mixer.update(clock.getDelta());
+      renderer.render(scene, camera);
+    }
+
+    animate();
+    waveAction.play();
+  }
+  useEffect(() => {
+    loadModel();
+  }, []);
+
   return (
-    <div className="App">
-      <Canvas>
-        <Suspense fallback={null}>
-          <Model />
-        </Suspense>
-      </Canvas>
+    <div id="avatar-container" className="w-full h-full">
+      <div id="avatar-loading"></div>
     </div>
   );
 }
