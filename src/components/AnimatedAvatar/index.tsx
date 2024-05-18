b0b19a1b8da4bbc6fc5ac001a6cf9d7f25f156f9
utkarsh <r.utkarsh.0010@gmail.com>
2024-05-18 16:08:47 +0530

    show static image while model is loading

--- src/components/AnimatedAvatar/index.tsx
+++ src/components/AnimatedAvatar/index.tsx
@@ -1,9 +1,11 @@
 "use client";
+import Image from "next/image";
 import { useEffect } from "react";
+import HoverContent from "../HoverContent";
 import { loadModel } from "./functions";
 import useHover from "@/hooks/useHover";
-import HoverContent from "../HoverContent";
 import { HOVER_MODEL_TEXT } from "@/constants/common";
+import CodingBoy from "@/assets/page/coding-boy.webp";
 
 export default function AnimatedAvatar() {
   const { hoverRef, isHovered } = useHover();
@@ -15,7 +17,15 @@ export default function AnimatedAvatar() {
   return (
     <>
       <div id="avatar-container" ref={hoverRef} className="w-full h-full">
-        <div id="avatar-loading"></div>
+        <div id="avatar-loading">
+          <Image
+            src={CodingBoy.src}
+            alt=""
+            width={700}
+            height={700}
+            loading="eager"
+          />
+        </div>
       </div>
       {isHovered && <HoverContent text={HOVER_MODEL_TEXT} />}
     </>
