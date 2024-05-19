4bca181fe0e6f564750eef052c941de613d58c12
utkarsh <r.utkarsh.0010@gmail.com>
2024-05-19 10:37:32 +0530

    add patch for 504 error code issue

--- src/containers/home/GithubStats.tsx
+++ src/containers/home/GithubStats.tsx
@@ -92,7 +92,7 @@ function GithubStats() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchStats();
+      const data = await fetchStats() || [];
       console.log(`data -- `, data);
       const activityDateArray: Array<string> = Object.keys(data).sort(
         (date1, date2) => new Date(date1).getTime() - new Date(date2).getTime()
