eb5eeb9f60d8a26ed27c14de087634ca957466d2
utkarsh <r.utkarsh.0010@gmail.com>
2024-06-08 13:38:08 +0530

    add smartpile

---
+++ src/components/solitaire/SmartPile.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useDrop } from "react-dnd";
+import Pile from "./components/Pile";
+import Card from "./components/Card";
+import DraggableCard from "./DraggableCard";
+import { SUITS, RANKS, COLORS } from "@/constants/solitaire";
+import last from "lodash/last";
+
+const pileTarget = {
+  drop(props, monitor) {
+    props.moveCards(monitor.getItem());
+  },
+
+  canDrop(props, monitor) {
+    const { rank } = monitor.getItem();
+    return rank === last(RANKS) && props.cards.length === 0;
+  },
+};
+
+function collect(monitor) {
+  return {
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop(),
+    suit: monitor.getItem() && monitor.getItem().suit,
+  };
+}
+
+const SmartPile = ({ cards, moveCards, index }) => {
+  const [{ isOver, canDrop, suit }] = useDrop(
+    "DraggableCard",
+    pileTarget,
+    collect
+  );
+
+  const renderedCards = cards.map((card, index, array) => {
+    if (card.upturned) {
+      return (
+        <DraggableCard
+          key={card.suit + card.rank}
+          {...card}
+          isLast={index === array.length - 1}
+          index={index}
+          upturned
+          where={["PILE", index]}
+        />
+      );
+    } else {
+      return <Card key={card.suit + card.rank} {...card} />;
+    }
+  });
+
+  return (
+    <div>
+      <Pile isOver={isOver} canDrop={canDrop} color={COLORS[suit]}>
+        {renderedCards}
+      </Pile>
+    </div>
+  );
+};
+
+export default SmartPile;
